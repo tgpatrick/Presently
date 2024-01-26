6d0a988818ba805d13d67e82bf057f54aa86e90a
Thomas Patrick <thomasgpatrick0203@gmail.com>
2024-01-26 10:36:32 -1000

    Add ability to block progression in OboardingView Also, stub out early Exchange onboarding

---
+++ Presently/UI Layer/Views/Onboarding/ExchangeItems/OnboardExchangeNameView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardExchangeNameView.swift
+//  Presently
+//
+//  Created by Thomas Patrick on 1/24/24.
+//
+
+import SwiftUI
+
+struct OnboardExchangeNameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardExchangeNameView()
+}
